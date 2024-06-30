fb8ca421749b1ca5d26ea6738b558faf83a5e91d
Jhenaidha Sadar her power project web Section <mustakali444888@gmail.com>
2024-06-30 15:56:52 +0600

    object js

--- firstcode.js
+++ firstcode.js
@@ -1,15 +1,2 @@
 
 
-var myVillage ='9756865486'
-console.log(typeof myVillage)
-
-var mobName = 06865432;
-console.log(typeof mobName)
-
-var boolitype = true;
-console.log(boolitype)
-
-
-var moontype =false;
-console.log( typeof moontype)
-
